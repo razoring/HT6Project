f76ee05e03a81f9a91cfcc2aeea6fa09fcea4070
raymondso123 <blotzytgaming@gmail.com>
2026-07-19 05:17:28 -0400

    chore: remove obsolete scratch script containing legacy API test code

--- scratch.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-import os
-
-key = "sk_1384d47cc749035ea0f6d9b82426ffacd4a5e2d3f5fd8a35"
-url = "https://api.elevenlabs.io/v1/voices"
-headers = {"xi-api-key": key}
-resp = requests.get(url, headers=headers)
-print(resp.text)
